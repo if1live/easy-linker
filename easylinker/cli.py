cabdec4ee4d545e296bd13a82ecafc43e21830e7
if1live <libsora25@gmail.com>
2014-12-12 18:24:56 +0900

    add facade function

--- easylinker/cli.py
+++ easylinker/cli.py
@@ -3,12 +3,24 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 import config
+import parsers
 
 def main():
-    # TODO dispath command if many command exist
-    print(config.PREDEFINED_VARIABLE_TABLE)
-
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        filename = parsers.to_unicode(filename)
+        parsers.run(filename)
+    else:
+        msg = 'Usage: {} <metadata>'.format(sys.argv[0])
+        print(msg)
+        print('\nPredefined Variables')
+        for k, v in config.PREDEFINED_VARIABLE_TABLE.items():
+            print('{}\t: {}'.format(k, v))
 
 if __name__ == '__main__':
     main()
