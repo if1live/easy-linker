fcddea2f0e72123dc66cd7b5143dc4913923bd05
if1live <libsora25@gmail.com>
2014-12-14 20:52:15 +0900

    on windows, use symbolic link to file. #2

--- easylinker/links.py
+++ easylinker/links.py
@@ -6,6 +6,24 @@ from __future__ import print_function
 import os
 
 
+def symlink(source, link_name):
+    """
+    http://stackoverflow.com/questions/6260149/os-symlink-support-in-windows
+    """
+    import os
+    os_symlink = getattr(os, "symlink", None)
+    if callable(os_symlink):
+        os_symlink(source, link_name)
+    else:
+        import ctypes
+        csl = ctypes.windll.kernel32.CreateSymbolicLinkW
+        csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
+        csl.restype = ctypes.c_ubyte
+        flags = 1 if os.path.isdir(source) else 0
+        if csl(link_name, source, flags) == 0:
+            raise ctypes.WinError()
+
+
 class LinkException(Exception):
     pass
 
@@ -54,7 +72,6 @@ class Link(object):
         except OSError:
             pass
 
-
         if os.name == 'nt':
             self._windows_file_link(src, dst)
         else:
@@ -64,8 +81,7 @@ class Link(object):
         return os.symlink(src, dst)
 
     def _windows_file_link(self, src, dst):
-        from ntfsutils import hardlink
-        return hardlink.create(src, dst)
+        symlink(src, dst)
 
     def _windows_directory_link(self, src, dst):
         from ntfsutils import junction
